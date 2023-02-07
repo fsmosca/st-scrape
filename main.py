07cbc133f40f211ae8f9ac45b65ff1ae28c72676
fsmosca <fsmosca@users.noreply.github.com>
2023-02-07 09:17:51 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+"""Scrapes quotes from the given url.
+
+The url is 'https://quotes.toscrape.com/'.
+"""
+
+import streamlit as st
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service as ChromeService
+import pandas as pd
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--window-size=1920x1080')
+chrome_options.add_argument('--disable-gpu')
+
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
+
+url = 'https://quotes.toscrape.com/'
+driver.get(url)
+
+quotes = driver.find_elements('xpath', '//span[@class="text"]')
+
+data = []
+for q in quotes:
+    t = q.text
+    data.append(t)
+
+df = pd.DataFrame(data, columns=['Quotes'])
+st.dataframe(df)
+
+driver.quit()
